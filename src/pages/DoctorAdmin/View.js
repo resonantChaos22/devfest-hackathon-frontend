a281fcf8a66569722f86e4c74dfd78c11f1c4f9d
kanak1411 <kanak1411@gmail.com>
2020-07-12 22:21:49 +0530

    created doctor side

--- src/pages/DoctorAdmin/View.js
+++ src/pages/DoctorAdmin/View.js
@@ -2,7 +2,16 @@ import React from 'react';
 import NavBar from '../../components/Nav/Nav'
 import SidebarExampleTransitions from '../../components/SideBar/SideBar';
 import axios from 'axios';
+import faker from 'faker';
 class View extends React.Component{
+    state={name:'',address:''}
+    componentDidMount() {
+        axios.get(`https://jsonplaceholder.typicode.com/users/${this.props.match.params.id}`).then(
+            response => {
+                this.setState({ name: response.data.name ,address:response.data.address.suite});
+            }
+        )
+    }
     render() {
         return (
             <div>
@@ -11,9 +20,23 @@ class View extends React.Component{
                     <div className=" two wide column">
                         <SidebarExampleTransitions />
                         </div>
-                </div>
-                </div>
-        );
+                    <div class="ui segment  fourteen wide column">
+                        <div className="ui grid"> 
+  <img class="ui small left floated image" src={faker.image.avatar()}>
+                        </img>
+                        <p>{this.state.name}</p>
+                        <p>{this.state.address} lorem100vdjbhjgvhfvbhjknkhgvhcvjhbknlnjkhvjgchgvhjbknlhgvjb m,mjhugvjb nm,kjuygvbnjuyughvbnjuytugfhvbnmkjuyiturydgxcvbnuytrydfcvhbnjuytuyfhgvbnjkiuyitufhcvnbnjuygh</p>
+                        </div>
+                        <br>
+                        </br>
+                        <br/>
+                        <div className="ui container">
+                            defrgtfhjmhngfd
+</div>
+                    </div>
+                    
+                                </div>
+        </div>);
 }
 }
 export default View;
